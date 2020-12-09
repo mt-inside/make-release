17c1276b175b60fd1e5e179c5b3dd7b0f35b3225
Matt Turner <matturner@gmail.com>
2020-12-09 00:45:06 +0000

    Fancy syntax

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ const token = core.getInput('token', { required: true });
 const tag = core.getInput('tag', { required: true });
 const body = core.getInput('body', { required: false }); // can push a 2nd tag on the same commit, leading to an "" body, which JS thinks is missing. Defaults to "" ?
 
-//const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
-const repoOwner = github.context.payload.repository.owner.login
-const repoName = github.context.payload.repository.name
+const repo = {
+  owner: github.context.payload.repository.owner.login,
+  name: github.context.payload.repository.name
+}
 
 const octokit = github.getOctokit(token);
 
 octokit.repos.createRelease({
-    //...context.repo,
-   owner: repoOwner,
-   repo: repoName,
-    tag_name: tag,
+  ...repo,
+  tag_name: tag,
   name: tag,
   body: body,
-    draft: true,
+  draft: true,
 });
