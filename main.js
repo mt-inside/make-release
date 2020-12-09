c8fbc0f80b468c94277af6b393a7025d55f05d7c
Matt Turner <matturner@gmail.com>
2020-12-09 00:37:32 +0000

    get repo details correctly

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ const tag = core.getInput('tag', { required: true });
 const body = core.getInput('body', { required: false }); // can push a 2nd tag on the same commit, leading to an "" body, which JS thinks is missing. Defaults to "" ?
 
 //const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
+const repoOwner = github.context.payload.repository.owner.login
+const repoName = github.context.payload.repository.name
 
 const octokit = github.getOctokit(token);
 
 octokit.repos.createRelease({
-    ...context.repo,
-  // owner: repoOwner,
-  // repo: repoName,
+    //...context.repo,
+   owner: repoOwner,
+   repo: repoName,
     tag_name: tag,
   name: tag,
   body: body,
